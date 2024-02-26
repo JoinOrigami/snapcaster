872d31157e6b071a3ceced0f6141207daa960a03
arsh <contact@arsh.me>
2024-02-26 09:46:54 -0800

    add fastify/secure-session

---
+++ server/db/index.ts
@@ -0,0 +1,12 @@
+import { Pool } from "pg";
+import { Kysely, PostgresDialect } from "kysely";
+
+export function init() {
+  const dialect = new PostgresDialect({
+    pool: new Pool({
+      connectionString: process.env.DATABASE_URL,
+      max: 10,
+    }),
+  });
+
+export const db = new Kysely<Database>({ dialect });
--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,6 @@
 import fastify from "fastify";
 import sensible from "@fastify/sensible";
+import session from "@fastify/secure-session";
 import ajvFormats from "ajv-formats";
 
 import routes from "./routes";
@@ -9,13 +10,23 @@ const server = fastify({
 });
 
 server.register(sensible);
+server.register(session, {
+  key: Buffer.from(process.env.SESSION_KEY as string, "base64"),
+  cookie: {
+    path: '/',
+    httpOnly: true,
+    secure: false,
+    maxAge: 3600 * 24 * 7 * 1000,
+  },
+});
+
 server.register(routes);
 
 (async () => {
   try {
-    await server.listen({ port: 3001 });
+    await server.listen({ host: "0.0.0.0", port: 3001 });
   } catch (err) {
-    server.log.error(err);
+    console.log(err);
     process.exit(1);
   }
 })();
