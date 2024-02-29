0dd70b0dc224580042fd6e07d5e9a1a73bbbc235
Stephen Caudill <scaudill@gmail.com>
2024-02-29 14:32:34 -0700

    Restyled the create proposal init frame

--- packages/server/routes/index.ts
+++ packages/server/routes/index.ts
@@ -1,10 +1,16 @@
+import path from "path";
 import type { FastifyInstance } from "@snapcaster/server/types/fastify";
 
+import fastifyStatic from "@fastify/static";
 import auth from "./auth";
 import images from "./images";
 import proposal from "./proposal";
 
 async function routes(fastify: FastifyInstance) {
+  fastify.register(fastifyStatic, {
+    root: path.join(__dirname, "..", "images"),
+    prefix: "/images/",
+  });
   fastify.register(auth);
   fastify.register(images);
   fastify.register(proposal);
