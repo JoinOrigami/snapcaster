483d7c01272521bdfb933d172931dbf02ffcef86
Stephen Caudill <scaudill@gmail.com>
2024-03-01 11:56:38 -0700

    Couple of small fixes

--- packages/client/pages/signin.tsx
+++ packages/client/pages/signin.tsx
@@ -5,7 +5,9 @@ function Page() {
   const router = useRouter();
   return (
     <div className="h-screen flex flex-col items-center justify-center">
-      <SignIn onSuccess={() => router.push(router.query.next as string || "/")} />;
+      <SignIn
+        onSuccess={() => router.push((router.query.next as string) || "/")}
+      />
     </div>
   );
 }
